8629baccd4396a7eb42b47bdff5911fb09c340f2
AluraFilme <allanbrosco@gmail.com>
2025-03-17 16:28:26 -0300

    Salvar inclusao do arquivo ci.yml

--- src/main/java/com/jwtvalidation/JwtValidator.java
+++ src/main/java/com/jwtvalidation/JwtValidator.java
@@ -3,10 +3,13 @@ package com.jwtvalidation;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.exceptions.JWTDecodeException;
 import com.auth0.jwt.interfaces.Claim;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Map;
 
 public class JwtValidator {
+    private static final Logger logger = LoggerFactory.getLogger(JwtValidator.class);
     public static boolean isValidJwt(String token) {
         try {
             var decodedJwt = JWT.decode(token);
@@ -14,18 +17,23 @@ public class JwtValidator {
 
             // Valida o número de claims
             if (claims.size() != 3) return false;
+            logger.info("Validando o token: {}", token);
+            logger.info("Validando o token: {}", token);
 
             // Valida a claim "Name" (não pode conter números e máximo de 256 caracteres)
             String name = claims.get("Name").asString();
             if (name == null || name.matches(".*\\d.*") || name.length() > 256) return false;
+            logger.info("Validando o token: {}", token);
 
             // Valida a claim "Role" (Admin, Member ou External)
             String role = claims.get("Role").asString();
             if (role == null || (!role.equals("Admin") && !role.equals("Member") && !role.equals("External"))) return false;
+            logger.info("Validando o token: {}", token);
 
             // Valida a claim "Seed" (número primo)
             String seedStr = claims.get("Seed").asString();
             if (seedStr == null || !PrimeNumberUtil.isPrime(Long.parseLong(seedStr))) return false;
+            logger.info("Validando o token: {}", token);
 
             return true;
         } catch (JWTDecodeException | NumberFormatException e) {
